fca4cb94e5dbb115bda1dcce53a686a202895c13
singlashashwat <50321436+singlashashwat@users.noreply.github.com>
2020-04-18 15:38:05 +0800

    Add files via upload

---
+++ src/reducers/index.js
@@ -0,0 +1,16 @@
+import { combineReducers } from 'redux';
+import produce from "immer";
+import Papa from 'papaparse';
+
+
+const app = (state = {
+  activeElements: []
+}, action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+
+}
+
+export default app
